9efa517849116d18f92ad0622188681e2fa29e7c
mekalanischala1122 <mekalanischala1122@gmail.com>
2022-06-28 15:55:18 +0530

    heroku port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var requests = require('./controllers/requests')
 requests(app);
 
 /*listening to port 3000*/
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log("listening to Port "+ PORT)
 });
